f1f0c5e3ef7914e20e368fb2072f2916d259d0cf
szutsb <szutsb@ulyssys.hu>
2021-08-04 14:55:24 +0200

    Reflection demo

--- animal-frontend/src/main/java/hu/ulyssys/java/course/maven/rest/CoreRestService.java
+++ animal-frontend/src/main/java/hu/ulyssys/java/course/maven/rest/CoreRestService.java
@@ -10,6 +10,8 @@ import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
 import java.util.stream.Collectors;
 
 public abstract class CoreRestService<T extends AbstractAnimal, M extends CoreRestModel> {
@@ -82,7 +84,25 @@ public abstract class CoreRestService<T extends AbstractAnimal, M extends CoreRe
         return model;
     }
 
-    protected abstract T initNewEntity();
+    //Generikus típus megszerzés, és reflection alapú objektum inicializálása
+    protected T initNewEntity() {
+
+        try {
+            // A konténer, beinjectáláskor, egy Proxy obejktumot hoz létre, ezért kérszer kell leolvasnunk ebben az esetben a ősosztály, és annak típusát
+            // Ha model paraméterre szükség, akkor 1 indexű elem kellene az array-ből
+            Class<T> type = (Class<T>) (((ParameterizedType)((Class)getClass().getGenericSuperclass()).getGenericSuperclass())).getActualTypeArguments()[1];
+            return type.getConstructor().newInstance();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     protected abstract M initNewModel();
 
